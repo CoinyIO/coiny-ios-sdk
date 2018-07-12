11b85f0a291043c74379c0d9d7977312af0b8faa
Kemal Kocabiyik <kkocabiyik@me.com>
2018-07-12 12:43:25 +0300

    Initial Commit

---
+++ CoinySDK/CoinySDK.h
@@ -0,0 +1,19 @@
+//
+//  CoinySDK.h
+//  CoinySDK
+//
+//  Created by KEMAL KOCABIYIK on 12.07.2018.
+//  Copyright © 2018 KEMAL KOCABIYIK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoinySDK.
+FOUNDATION_EXPORT double CoinySDKVersionNumber;
+
+//! Project version string for CoinySDK.
+FOUNDATION_EXPORT const unsigned char CoinySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoinySDK/PublicHeader.h>
+
+
